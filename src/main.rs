bf6dda55a489087910ac4f087f39212f33286928
Deadbeef <ent3rm4n@gmail.com>
2023-04-22 02:13:13 +0800

    typst render to image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,92 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::fs::File;
+use std::{io, fs};
+use std::path::Path;
+
+use comemo::Prehashed;
+use typst::diag::{FileResult, FileError};
+use typst::eval::Library;
+use typst::font::{FontBook, Font};
+use typst::geom::Color;
+use typst::syntax::{SourceId, Source};
+use typst::util::Buffer;
+
+pub struct MyWorld {
+    library: Prehashed<Library>,
+    fonts: Vec<Font>,
+    book: Prehashed<FontBook>,
+    main: Source,
+}
+
+impl MyWorld {
+    pub fn new(src: String)  -> io::Result<Self> {
+        //let fonts_list = Command::new("fc-list").arg("-f").arg("%{file}\n").output()?;
+        //let s = String::from_utf8_lossy(&fonts_list.stdout);
+        let s = "/nix/store/ad3l14p8hmq980blhb7a6fqbd9gsic78-dejavu-fonts-2.37/share/fonts/truetype/DejaVuMathTeXGyre.ttf";
+        let fonts = s.lines().flat_map(|x| {
+            fs::read(x).map(|data| Font::iter(data.into())).ok()
+        }).flatten().collect::<Vec<_>>();
+
+        dbg!(&fonts);
+
+        println!("{:?}", fonts[0].ttf().tables().math);
+
+        let book = FontBook::from_fonts(fonts.iter());
+        Ok(Self {
+            library: Prehashed::new(typst_library::build()),
+            fonts,
+            book: Prehashed::new(book),
+            main: Source::new(SourceId::from_u16(0), Path::new("input"), src),
+        })
+    }
+}
+
+impl typst::World for MyWorld {
+    fn library(&self) ->  &Prehashed<Library> {
+        &self.library
+    }
+
+    fn font(&self, id: usize) -> Option<Font> {
+        self.fonts.get(id).cloned()
+    }
+
+    fn book(&self) ->  &Prehashed<typst::font::FontBook> {
+        &self.book
+    }
+
+    fn resolve(&self, path: &Path) -> FileResult<SourceId> {
+        Err(FileError::NotFound(path.into()))
+    }
+
+    fn file(&self, path: &Path) -> FileResult<Buffer> {
+        Err(FileError::NotFound(path.into()))
+    }
+
+    fn source(&self, id: SourceId) -> &Source {
+        assert_eq!(0, id.into_u16());
+        &self.main
+    }
+
+    fn main(&self) -> &Source {
+        &self.main
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let world = MyWorld::new(r#"
+        #show math.equation: set text(font: "DejaVu Math TeX Gyre")
+        $a^2 + b^2 = c^2$
+    "#.into())?;
+    println!("debug uwu");
+    let input = typst::compile(&world).unwrap();
+    let pixmap = typst::export::render(&input.pages[0], 5.0, Color::WHITE);
+    image::write_buffer_with_format(
+        &mut File::create("./out.png")?,
+        bytemuck::cast_slice(pixmap.pixels()),
+        pixmap.width(),
+        pixmap.height(),
+        image::ColorType::Rgba8,
+        image::ImageFormat::Png
+    )?;
+    Ok(())
 }
